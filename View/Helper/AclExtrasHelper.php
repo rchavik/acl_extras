8ee1dce30bfba2743d5d2fca89b4899a79a6413c
Rachman Chavik <rchavik@xintesa.com>
2012-01-12 13:00:11 +0700

    set block and + only send for authenticated users

--- View/Helper/AclExtrasHelper.php
+++ View/Helper/AclExtrasHelper.php
@@ -4,11 +4,12 @@ class AclExtrasHelper extends AppHelper {
 
 	public $helpers = array(
 		'Html',
+		'Session',
 		);
 
 	public function beforeRender() {
-		if (isset($this->params['admin'])) {
-			$this->Html->script('/acl_extras/js/acl_permissions.js', false);
+		if (isset($this->params['admin']) && $this->Session->read('Auth.User.id')) {
+			$this->Html->script('/acl_extras/js/acl_permissions.js', array('inline' => false, 'block' => 'scriptBottom'));
 			$this->Html->css('/acl_extras/css/admin', false, array('inline' => false));
 		}
 	}
