41487fa6554b378165d6aa4230997476eee06273
Rachman Chavik <rchavik@xintesa.com>
2012-01-25 10:40:08 +0700

    AclExtrasActivation extends CroogoAclPluginActivation

--- Config/AclExtrasActivation.php
+++ Config/AclExtrasActivation.php
@@ -1,31 +1,7 @@
 <?php
 
-class AclExtrasActivation {
+App::uses('CroogoAclPluginActivation', 'Croogo');
 
-	public function beforeActivation($controller) {
-		return true;
-	}
-
-	public function onActivation($controller) {
-	}
-
-	public function beforeDeactivation($controller) {
-		if (Configure::read('Site.acl_plugin') == 'AclExtras') {
-			// plugin in use
-			return false;
-		}
-		$loaded = CakePlugin::loaded();
-		$leftovers = array_diff($loaded, array('AclExtras'));
-		foreach ($leftovers as $plugin) {
-			// only allow deactivation when alternate Acl plugin is active
-			if (preg_match('/^Acl/', $plugin)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public function onDeactivation($controller) {
-	}
+class AclExtrasActivation extends CroogoAclPluginActivation {
 
 }
