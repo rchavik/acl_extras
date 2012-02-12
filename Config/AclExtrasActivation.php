cae6978a65354529754baf304de2b0c683451043
Rachman Chavik <rchavik@xintesa.com>
2012-02-12 19:49:40 +0700

    CroogoAclPluginActivation didn't make it upstream.

--- Config/AclExtrasActivation.php
+++ Config/AclExtrasActivation.php
@@ -1,7 +1,57 @@
 <?php
 
-App::uses('CroogoAclPluginActivation', 'Croogo');
+class AclExtrasActivation {
 
-class AclExtrasActivation extends CroogoAclPluginActivation {
+	public function beforeActivation($controller) {
+		return true;
+	}
 
+ public function onActivation($controller) {
+
+	$Setting = ClassRegistry::init('Setting');
+
+	$setting = $Setting->find('first', array(
+		'conditions' => array(
+			'key' => 'Site.acl_plugin',
+			),
+		));
+
+	if (empty($setting)) {
+		$setting = $Setting->create(array(
+			'key' => 'Site.acl_plugin',
+			'value' => Configure::read('Site.acl_plugin'),
+			'title' => 'Acl Plugin',
+			'description' => 'CamelCased Acl Plugin name to use. Make sure that plugin name is correct and active.',
+			'editable' => 1,
+			'weight' => 5,
+			));
+		$Setting->save($setting);
+		}
+	}
+
+	public function beforeDeactivation($controller) {
+		if (!empty($controller->params['pass'][0])) {
+			$plugin = $controller->params['pass'][0];
+		} else {
+			return false;
+		}
+
+		if (Configure::read('Site.acl_plugin') == $plugin) {
+			// plugin in use
+			return false;
+		}
+
+		$loaded = CakePlugin::loaded();
+		$leftovers = array_diff($loaded, array('Acl'));
+		foreach ($leftovers as $plugin) {
+			// only allow deactivation when alternate Acl plugin is active
+			if (preg_match('/^Acl/', $plugin)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function onDeactivation($controller) {
+	}
 }
