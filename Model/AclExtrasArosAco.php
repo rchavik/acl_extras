f17e73d4da42ce1b3e6dd2b0bc30798af2708658
Rachman Chavik <rchavik@xintesa.com>
2012-03-26 13:57:16 +0700

    tabify Model/*

--- Model/AclExtrasArosAco.php
+++ Model/AclExtrasArosAco.php
@@ -1,17 +1,17 @@
 <?php
 class AclExtrasArosAco extends AclExtrasAppModel {
 
-	var $useTable = 'aros_acos';
+	public $useTable = 'aros_acos';
 
-	var $belongsTo = array(
-	    'AclAro' => array(
-	        'className' => 'AclExtras.AclExtrasAro',
-	        'foreignKey' => 'aro_id',
-	    ),
-	    'AclAco' => array(
-	        'className' => 'AclExtras.AclExtrasAco',
-	        'foreignKey' => 'aco_id',
-	    ),
+	public $belongsTo = array(
+		'AclAro' => array(
+			'className' => 'AclExtras.AclExtrasAro',
+			'foreignKey' => 'aro_id',
+		),
+		'AclAco' => array(
+			'className' => 'AclExtras.AclExtrasAco',
+			'foreignKey' => 'aco_id',
+		),
 	);
 
 }
--- Model/Behavior/RoleAroBehavior.php
+++ Model/Behavior/RoleAroBehavior.php
@@ -2,26 +2,26 @@
 
 class RoleAroBehavior extends ModelBehavior {
 
-    public function parentNode($model) {
+	public function parentNode($model) {
 		$this->log('AroUpdater::parentNode');
-        if (!$model->id && empty($model->data)) {
-            return null;
-        } else {
-            $id = $model->id ? $model->id : $model->data['Role']['id'];
-            $aro = $model->Aro->find('first', array(
-                'conditions' => array(
-                    'model' => $model->alias,
-                    'foreign_key' => $id,
-                    )
-                ));
-            if (empty($aro['Aro']['foreign_key'])) {
-                $return = null;
-            } else {
-                $return = array('Role' => array('id' => $aro['Aro']['foreign_key']));
-            }
+		if (!$model->id && empty($model->data)) {
+			return null;
+		} else {
+			$id = $model->id ? $model->id : $model->data['Role']['id'];
+			$aro = $model->Aro->find('first', array(
+				'conditions' => array(
+					'model' => $model->alias,
+					'foreign_key' => $id,
+					)
+				));
+			if (empty($aro['Aro']['foreign_key'])) {
+				$return = null;
+			} else {
+				$return = array('Role' => array('id' => $aro['Aro']['foreign_key']));
+			}
 			return $return;
-        }
-    }
+		}
+	}
 
 	public function afterSave($model, $created) {
 		if (empty($model->data['Role']['alias'])) {
